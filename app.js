60aa5958347c0371a17a6b89696f8c456cea879a
Shreyash <shreyashkumar95@gmail.com>
2019-09-01 16:07:01 +0530

    Callback abstraction: Moving towards asynchronous programming

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const game1 = new Hangman('shreyash thakur', 4);
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
 
-
 puzzleEl.textContent = game1.puzzle;
 guessesEl.textContent = game1.statusMessage;
 
+getPuzzle((error, puzzle) => {
+    if(error){
+        console.log("An error occured while fetching the puzzle, ", error);
+    }
+    else{
+        console.log(puzzle);
+    }
+});
 
 
 
